af2a69ad28d5fe009ada798af547dc07d084800c
Matt Andreko <mandreko@gmail.com>
2011-08-20 14:49:25 -0400

    Solved problem 37

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,6 +1,7 @@
 require 'mathn'
 
 class Integer
+  
   def is_circular_prime?
     nums = self.to_s.chars.to_a
     nums.length.times do
@@ -9,8 +10,36 @@ class Integer
       nums.push nums[0]
       nums.shift
     end
-
   true
-
+  end
+  
+  def truncatable_left?
+    self_str = self.to_s.chars.to_a
+    
+    (0..self_str.length-1).each do |i|
+      #puts self_str.join
+      if !self_str.join.to_i.prime?
+        return false
+      end
+      self_str.shift
+    end
+    return true
+  end
+  
+  def truncatable_right?
+    self_str = self.to_s.chars.to_a
+    
+    (0..self_str.length-1).each do |i|
+      #puts self_str.join
+      if !self_str.join.to_i.prime?
+        return false
+      end
+      self_str.pop
+    end
+    return true
+  end
+  
+  def truncatable?
+    self.truncatable_left? && self.truncatable_right?
   end
 end
---
+++ problems/problem037.rb
@@ -0,0 +1,40 @@
+# Euler 37
+# http://projecteuler.net/index.php?section=problems&id=37
+# The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, 
+# and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+# 
+# Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+# 
+# NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+require 'benchmark'
+require 'mathn'
+require '../lib/prime.rb'
+
+time = Benchmark.realtime do
+  
+  truncatable_primes = []
+  count = 0
+  
+  Prime.each do |prime|
+    
+    # Don't use 2,3,5,7 - just skip the loop
+    if (prime == 2 || prime == 3 || prime == 5 || prime ==7)
+      next
+    end
+    
+    if prime.truncatable?
+      truncatable_primes << prime
+      puts prime
+    end
+    
+    if truncatable_primes.count == 11
+      break
+    end
+    
+    
+  end
+  
+  @answer = truncatable_primes.inject(:+)
+  
+end
+puts "Euler 37: #{@answer}\t#{time*1000} milliseconds"
