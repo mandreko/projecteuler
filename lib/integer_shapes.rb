c89f765258f173d0087e6e59fff519cb4eb81be2
Matt Andreko <mandreko@gmail.com>
2011-08-24 13:20:18 -0400

    solved 44 added some library functions to the integer_shapes

--- lib/integer_shapes.rb
+++ lib/integer_shapes.rb
@@ -6,9 +6,32 @@ class Integer
   def pentagonal
     self * (3 * self - 1) / 2
   end
-
   def hexagonal
     self * (2 * self - 1)
   end
 
+  def triangle?
+    self == Triangular.take_while {|x| x <= self }.max
+  end
+  def pentagonal?
+    (Math.sqrt(24 * self + 1) + 1) % 6 == 0
+  end
 end
+
+Triangular = Enumerator.new {|x, i = 0|
+  loop {
+    x << (i += 1).triangular
+  }
+}
+
+Pentagonal = Enumerator.new {|x, i = 0|
+  loop {
+    x << (i += 1).pentagonal
+  }
+}
+
+Hexagonal = Enumerator.new {|x, i = 0|
+  loop {
+    x << (i += 1).hexagonal
+  }
+}
