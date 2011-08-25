d7801e8413ed7ec5869fd48128853aa6a24bd06e
Matt Andreko <mandreko@gmail.com>
2011-08-25 10:10:56 -0400

    solved 59

---
+++ problems/problem059.rb
@@ -0,0 +1,42 @@
+# Euler 59
+# http://projecteuler.net/index.php?section=problems&id=59
+# Each character on a computer is assigned a unique code and the preferred standard is ASCII (American Standard Code
+# for Information Interchange). For example, uppercase A = 65, asterisk (*) = 42, and lowercase k = 107.
+#
+# A modern encryption method is to take a text file, convert the bytes to ASCII, then XOR each byte with a given value,
+# taken from a secret key. The advantage with the XOR function is that using the same encryption key on the cipher text,
+# restores the plain text; for example, 65 XOR 42 = 107, then 107 XOR 42 = 65.
+#
+# For unbreakable encryption, the key is the same length as the plain text message, and the key is made up of random
+# bytes. The user would keep the encrypted message and the encryption key in different locations, and without both
+# "halves", it is impossible to decrypt the message.
+#
+# Unfortunately, this method is impractical for most users, so the modified method is to use a password as a key. If the
+# password is shorter than the message, which is likely, the key is repeated cyclically throughout the message. The
+# balance for this method is using a sufficiently long password key for security, but short enough to be memorable.
+#
+# Your task has been made easy, as the encryption key consists of three lower case characters. Using cipher1.txt (right
+# click and 'Save Link/Target As...'), a file containing the encrypted ASCII codes, and the knowledge that the plain
+# text must contain common English words, decrypt the message and find the sum of the ASCII values in the original text.
+
+require 'benchmark'
+
+time = Benchmark.realtime do
+  File.open(File.expand_path(File.dirname(__FILE__)) + "/../inputs/problem059.txt", "r") do |infile|
+    chars = infile.readline.split(',').to_a.map(&:to_i)
+    letter_permutations = ("a".."z").to_a.permutation(3)
+    letter_permutations.each do |perm|
+      str = String.new
+      perm_ord = perm.map(&:ord)
+      chars.each_with_index do |value, key|
+        str += (value^perm_ord[key%3].to_i).chr
+      end
+
+      if str.match /^[0-9a-zA-Z\s\.,\(\)!;\-']*$/
+        @answer = str.chars.to_a.map(&:ord).inject(:+)
+        break
+      end
+    end
+  end
+end
+puts "Euler 59: #{@answer}\t#{time*1000} milliseconds"
